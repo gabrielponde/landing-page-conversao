ba02c66ec91cf80bd59d9e6f60a8e4d72f08471c
Gabriel Avena <gabrielpavena@gmail.com>
2025-03-26 09:13:50 -0300

    Ajuste de aspas duplas

--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -5,19 +5,19 @@ const Testimonial: React.FC = () => {
     <section id="testimonials" className="testimonials">
       <h2>O que nossos alunos dizem:</h2>
       <div className="testimonial">
-        <p> ⭐️⭐️⭐️⭐️⭐️ "O curso foi excelente! Em poucas semanas, já estava desenvolvendo meu primeiro projeto em Java. Recomendo muito!"</p>
+        <p> ⭐️⭐️⭐️⭐️⭐️ &quot;O curso foi excelente! Em poucas semanas, já estava desenvolvendo meu primeiro projeto em Java. Recomendo muito!&quot;</p>
         <span>- João Silva, Desenvolvedor</span>
       </div>
       <div className="testimonial">
-        <p> ⭐️⭐️⭐️⭐️⭐️ "A didática do eBook e dos exemplos práticos fez toda a diferença na minha aprendizagem!"</p>
+        <p> ⭐️⭐️⭐️⭐️⭐️ &quot;A didática do eBook e dos exemplos práticos fez toda a diferença na minha aprendizagem!&quot;</p>
         <span>- Maria Oliveira, Estudante de TI</span>
       </div>
       <div className="testimonial">
-        <p> ⭐️⭐️⭐️⭐️⭐️ "O conteúdo do curso me proporcionou o conhecimento necessário para meu primeiro estágio como desenvolvedor Java. Agradeço muito!"</p>
+        <p> ⭐️⭐️⭐️⭐️⭐️ &quot;O conteúdo do curso me proporcionou o conhecimento necessário para meu primeiro estágio como desenvolvedor Java. Agradeço muito!&quot;</p>
         <span>- Carlos Souza, Estagiário de TI</span>
       </div>
       <div className="testimonial">
-        <p> ⭐️⭐️⭐️⭐️⭐️ "Eu não sabia nada sobre programação antes desse curso, e hoje já desenvolvo aplicações profissionais. Vale muito a pena!"</p>
+        <p> ⭐️⭐️⭐️⭐️⭐️ &quot;Eu não sabia nada sobre programação antes desse curso, e hoje já desenvolvo aplicações profissionais. Vale muito a pena!&quot;</p>
         <span>- Ana Costa, Desenvolvedora Júnior</span>
       </div>
     </section>
